b532db4be3a58c937501f84d9a8ec9b23eb1bb3e
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-06 12:41:54 -0400

    add es6 var let const lesson complete

--- 01 var, let, const/app.js
+++ 01 var, let, const/app.js
@@ -1,1 +1,25 @@
-//
+//var let const
+
+//defining a var
+var number = 100;
+// console.log(number);
+//updating var
+number = 200;
+// console.log(number);
+//redefining var
+var number = 'orange';
+// console.log(number);
+
+//define let
+let amount = 100;
+// console.log(amount);
+//update let
+amount = 200;
+// console.log(amount);
+//redefine let
+// let amount = 'orange';
+
+//mutate const
+const person = { name: 'tim' };
+person.name = 'joe';
+console.log(person.name);
