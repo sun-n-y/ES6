fd5d3f30a86b2ec5ff6b84dd881dfa9346053ce3
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-06 14:46:34 -0400

    add es6 function scope block scope lesson complete

--- 02 function scope, block scope/app.js
+++ 02 function scope, block scope/app.js
@@ -1,1 +1,24 @@
-//
+//global scope variables
+var amount = 100;
+
+//function scope
+function greet() {
+  var random = 'some random value';
+  console.log(`hello there ${amount} ${random}`);
+}
+
+// greet();
+
+// console.log(random);
+
+let total = 1000;
+let test = true;
+
+if (test) {
+  let total = 'pranges';
+  let value = 'raie;f';
+  console.log(`hello there`);
+}
+
+console.log(total);
+console.log(value);
