677c563afcfcb07d691cd642642c18deffd3a100
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-07 11:21:24 -0400

    add es6 arrow functions lesson pt 1

--- 04 arrow functions/app.js
+++ 04 arrow functions/app.js
@@ -1,1 +1,47 @@
-//
+//arrow functions
+
+const sayHi = () => console.log('hello');
+
+sayHi();
+
+//passing in one parameter
+const double = (value) => value * 2;
+
+const num = double(4);
+
+console.log(num);
+
+//passing in two parameters, more than one line
+const multiply = (value1, value2) => {
+  const result = value1 * value2;
+  return { result };
+};
+
+console.log(multiply(3, 3));
+
+//return object
+const object = () => ({ name: 'john', age: 25 });
+
+console.log(object());
+
+//callback functions, one liner
+const numbers = [1, 2, 3, 4, 5, 6];
+
+const big = numbers.filter((number) => number > 2);
+
+console.log(big);
+
+//callback functions, two parameters and more than one line
+const numbers2 = [1, 2, 3, 4, 5, 6];
+
+const big2 = numbers.filter((number, index) => {
+  console.log(index);
+  return number > 2;
+});
+
+console.log(big2);
+
+//arrow funcitons in event listener
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', (e) => console.log(window));
