33fa626f7c7b333f78d7fcb8ec9db7e9b32c6fd3
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-07 11:48:58 -0400

    add es6 arrow functions lesson pt 3

--- 05 arrow functions 'this'/app.js
+++ 05 arrow functions 'this'/app.js
@@ -1,1 +1,38 @@
 //
+// const tom = {
+//   firstName: 'tom',
+//   lastName: 'smith',
+//   sayName: function () {
+//     console.log(this);
+//     const self = this;
+//     setTimeout(function () {
+//       console.log(self);
+//       console.log(`hello my name is ${self.firstName} ${self.lastName}`);
+//     }, 2000);
+//   },
+// };
+
+//use arrow funciton
+const tom = {
+  firstName: 'tom',
+  lastName: 'smith',
+  sayName: function () {
+    console.log(this);
+    setTimeout(() => {
+      console.log(this);
+      console.log(`hello my name is ${this.firstName} ${this.lastName}`);
+    }, 2000);
+  },
+};
+
+const joe = {
+  firstName: 'joe',
+  lastName: 'dean',
+  sayName: () => {
+    console.log(this);
+    console.log(`hello my name is ${this.firstName} ${this.lastName}`);
+  },
+};
+
+// tom.sayName();
+joe.sayName();
