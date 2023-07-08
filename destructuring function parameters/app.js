a032b53e5969c5d54e916109821070e9a70a68b6
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-08 15:40:55 -0400

    add es6 function destructing lesson complete

--- destructuring function parameters/app.js
+++ destructuring function parameters/app.js
@@ -1,1 +1,15 @@
 //
+//
+const tim = {
+  first: 'tim',
+  last: 'cook',
+  city: 'chicago',
+  siblings: { sister: 'jane' },
+};
+
+function printPerson({ first, last, city }) {
+  //   const { first, last, city } = person;
+  console.log(first, last, city);
+}
+
+printPerson(tim);
