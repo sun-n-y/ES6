7023450154208626474a97cae01e5483de7a7ece
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-08 15:57:28 -0400

    add es6 new string methods lesson complete

--- 12 new string methods/app.js
+++ 12 new string methods/app.js
@@ -1,1 +1,13 @@
 //
+const person = 'peter smith';
+const employee = '23456-EMP-PETER-SMITH';
+const manager = '23456-MAN-TIM-JONES';
+
+// console.log(person.startsWith('P'));
+// console.log(employee.startsWith('EMP', 6));
+
+const multiplyPeople = (person, amount = 5) => person.repeat(amount);
+
+const people = multiplyPeople(person);
+
+console.log(people);
