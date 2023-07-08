067d46452887784770b283a01c0b1cbf4f029773
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-08 13:08:32 -0400

    add es6 default parameters and gotchas complete

--- 07 arrow functions default parameters/app.js
+++ 07 arrow functions default parameters/app.js
@@ -1,1 +1,12 @@
 //
+const john = 'john';
+const peter = 'peter';
+
+function sayHi(person = 'polo') {
+  console.log(`Hi ${person}`);
+}
+
+const sayHello = (person = 'him') => console.log(`Hello ${person}`);
+
+sayHi();
+sayHello();
