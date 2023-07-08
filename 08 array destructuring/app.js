70e0ca41b0e2db3b1691659e2aa9ad634945f60d
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-08 14:30:21 -0400

    add es6 array destructuring lesson complete

--- 08 array destructuring/app.js
+++ 08 array destructuring/app.js
@@ -1,1 +1,15 @@
 //
+const fruits = ['orange', 'banana', 'lemon'];
+const friends = ['john', 'peter', 'tom', 'anana', 'kelly'];
+
+//old way of accessing vairables
+const orange = fruits[0];
+const banana = fruits[1];
+const lemon = fruits[2];
+
+console.log(orange, banana, lemon);
+
+//faster way
+const [john, tom, bob] = friends;
+
+console.log(john, tom, bob);
