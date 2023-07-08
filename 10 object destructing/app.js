1cbece35311638ca80640b069c4f9332133ccae3
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-08 15:21:45 -0400

    add es6 obeject destructing lesson complete

--- 10 object destructing/app.js
+++ 10 object destructing/app.js
@@ -1,1 +1,23 @@
 //
+const tim = {
+  first: 'tim',
+  last: 'cook',
+  city: 'chicago',
+  siblings: { sister: 'jane' },
+};
+
+//es6
+const {
+  first: firstName,
+  last,
+  city,
+  siblings: { sister: favSib },
+} = tim;
+console.log(firstName, last, city, favSib);
+
+//old way
+// const firstName = tim.first;
+// const lastName = tim.last;
+// const sister = tim.siblings.sister;
+
+// console.log(firstName, lastName, sister);
