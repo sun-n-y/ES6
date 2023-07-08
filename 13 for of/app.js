4bfe93e5b288fe9c4524b792617075c354ae06bb
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-08 16:12:35 -0400

    add es6 for of lesson complete

--- 13 for of/app.js
+++ 13 for of/app.js
@@ -1,1 +1,23 @@
 //
+const fruits = ['apple', 'orange', 'banana', 'peach'];
+const longName = 'John Smith Pepper III';
+let shortName = '';
+
+//
+for (const letter of longName) {
+  //   console.log(letter);
+  if (letter === ' ') {
+    continue;
+  } else {
+    shortName += letter;
+  }
+}
+
+// console.log(shortName);
+
+for (const fruit of fruits) {
+  if (fruit === 'banana') {
+    continue;
+  }
+  console.log(fruit);
+}
