94dab40340d514b9de6bbf51818e376c233a81c2
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-09 14:02:53 -0400

    add es6 array from DOM lesson complete

--- 21 array from DOM/app.js
+++ 21 array from DOM/app.js
@@ -1,1 +1,21 @@
 //
+
+const p = document.querySelectorAll('p');
+const result = document.getElementById('result');
+const second = document.getElementById('second');
+
+console.log(p);
+
+let newText = Array.from(p);
+
+result.innerHTML = newText
+  .map((item) => `<h1>${item.textContent}</h1>`)
+  .join(' ');
+
+const text = Array.from(document.querySelectorAll('p'), (item) => {
+  return `<span>${item.textContent}</span>`;
+}).join(' ');
+
+console.log(text);
+
+second.innerHTML = text;
