0c86b090f278fd3e92d963d2fb49ca2d7bacce7e
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-09 16:09:25 -0400

    add es6 array includes lesson complete

--- 28 array includes/app.js
+++ 28 array includes/app.js
@@ -1,1 +1,13 @@
 //
+
+const groceries = ['milk', 'bread', 'meat'];
+
+let randomItem = 'lemon';
+randomItem = 'milk';
+const isIncluded = groceries.includes(randomItem, 1);
+
+console.log(isIncluded);
+
+if (groceries.includes(randomItem)) {
+  console.log(`yeah! its on the list`);
+}
