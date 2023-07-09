bc7f741f9025891f1844bc19626e5214e8793617
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-09 11:29:43 -0400

    add es6 spread operator objects lesson complete

--- 15 spread operator objects/app.js
+++ 15 spread operator objects/app.js
@@ -1,1 +1,10 @@
 //
+const person = {
+  name: 'john',
+  job: 'developer',
+};
+
+const newPerson = { ...person, city: 'chicago', name: 'tom' };
+newPerson.food = 'pizza';
+console.log(newPerson);
+console.log(person);
