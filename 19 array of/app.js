14160a4152f2280d0e0661d8cf835c0dab9dc6cb
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-09 13:31:40 -0400

    add es6 array of lesson complete

--- 19 array of/app.js
+++ 19 array of/app.js
@@ -1,1 +1,10 @@
 //
+const example = ['one', 'two', 'three'];
+
+// console.log(example);
+// console.log(example.map);
+// console.log(example.from);
+// console.log(example.of);
+
+const friends = Array.of('john', 2, true);
+console.log(friends);
