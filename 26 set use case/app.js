6ef519e0fbac57c0f96a4b62b8604f1aefae1431
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-09 15:36:54 -0400

    add es6 set use case lesson complete

--- 26 set use case/app.js
+++ 26 set use case/app.js
@@ -1,1 +1,32 @@
 //
+const products = [
+  {
+    title: 'some title 1',
+    company: 'company 1',
+  },
+  {
+    title: 'some title 2',
+    company: 'company 2',
+  },
+  {
+    title: 'some title 3',
+    company: 'company 3',
+  },
+  {
+    title: 'some title 4',
+    company: 'company 2',
+  },
+];
+
+// const companies = products.map((item) => item.company);
+// console.log(companies);
+
+// const uniqueCompanies = new Set(companies);
+// console.log(uniqueCompanies);
+
+// const finalCompanies = [...uniqueCompanies];
+
+// console.log(finalCompanies);
+
+const result = ['all', ...new Set(products.map((item) => item.company))];
+console.log(result);
