788faacd2b8d1d1452a3f0e256f7725b85911779
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-09 11:23:15 -0400

    add es6 spread operator basics lesson complete

--- 14 spread operator basics/app.js
+++ 14 spread operator basics/app.js
@@ -1,1 +1,27 @@
-//
+// spread operator
+
+//strings
+const udemy = 'udemy';
+
+const letters = [...udemy];
+
+// console.log(letters);
+
+//arrays
+const boys = ['john', 'peter', 'tim'];
+const girls = ['susan', 'melissa'];
+const bestFriend = 'arnold';
+
+const friends = [bestFriend, ...boys, bestFriend, ...girls, bestFriend];
+
+console.log(friends);
+
+//refernce
+// const newFriends = friends;
+// newFriends[0] = 'karina';
+
+//copy
+const newFriends = [...friends];
+console.log(friends);
+newFriends[0] = 'karina';
+console.log(newFriends);
