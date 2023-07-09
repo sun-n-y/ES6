09e4f5e59a34f3e54d8e2e5516f3c0be1bc488ac
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-09 11:49:43 -0400

    add es6 spread operator DOM lesson complete

--- 16 spread operator DOM/app.js
+++ 16 spread operator DOM/app.js
@@ -1,1 +1,10 @@
 //
+
+const headings = [...document.querySelectorAll('h1')];
+const result = document.querySelector('#result');
+
+console.log(headings);
+
+result.innerHTML = headings
+  .map((item) => `<span>${item.textContent}</span>`)
+  .join(' ');
