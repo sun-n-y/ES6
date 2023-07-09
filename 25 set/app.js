6fada28a108d0b36fb4bc4a325c09a72553f395c
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-09 15:25:38 -0400

    add es6 set lesson complete

--- 25 set/app.js
+++ 25 set/app.js
@@ -1,1 +1,15 @@
 //
+const unique = new Set();
+
+unique.add('first');
+unique.add('second');
+unique.add(5);
+unique.add(6);
+
+const result = unique.delete(5);
+console.log(result);
+
+const isValue = unique.has(6);
+console.log(isValue);
+
+console.log(unique);
