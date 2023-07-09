6bb6c6452fcf642983bbaeb37f97f48ed6dc7fb1
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-09 15:56:08 -0400

    add es6 string includes lesson complete

--- 27 string includes/app.js
+++ 27 string includes/app.js
@@ -1,1 +1,23 @@
 //
+const products = [
+  { title: 'Modern Poster' },
+  { title: 'Bar Stool' },
+  { title: 'Armchair' },
+  { title: 'Leather Chair' },
+];
+
+const text = 'a';
+const filteredProduct = products.filter((product) =>
+  product.title.toLowerCase().includes(text)
+);
+console.log(filteredProduct);
+
+// const firstName = 'john';
+
+// // const result = firstName.includes('j', 1);
+
+// const product = { title: 'some title 1' };
+
+// const result = product.title.includes('s');
+
+// console.log(result);
