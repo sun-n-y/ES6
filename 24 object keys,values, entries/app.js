28e1cfbdbd184f03920dabd3b01d7c5090904d40
sunny patel <sunnykpatel.skp@gmail.com>
2023-07-09 15:16:26 -0400

    add es6 objects keys, values, entries lesson complete

--- 24 object keys,values, entries/app.js
+++ 24 object keys,values, entries/app.js
@@ -1,1 +1,30 @@
 //
+const person = {
+  name: 'john',
+  age: 25,
+  status: 'student',
+};
+
+//object keys
+const keys = Object.keys(person);
+console.log(keys);
+//object values
+const values = Object.values(person);
+console.log(values);
+//object enttries
+const both = Object.entries(person);
+console.log(both);
+
+//map
+const result = both.map((item) => {
+  const [first, second] = item;
+  return first;
+});
+
+// console.log(result);
+
+// for of
+for (const [first, second] of both) {
+  //   const [first, second] = item;
+  console.log(first, second);
+}
