01cbf6a225c11d65c3917100e785f652ea615903
sunny patel <sunnykpatel.skp@gmail.com>
2023-08-24 12:17:06 -0400

    add es6 function and block scope lesson complete

--- 30 funciton n block scope/app.js
+++ 30 funciton n block scope/app.js
@@ -1,1 +1,33 @@
-//
+//global scope
+
+var amount = 100;
+
+function greet() {
+  var random = 'some random value';
+  console.log(`hello there ${amount} ${random}`);
+}
+
+// greet();
+
+// console.log(random);
+
+// var total = 1000;
+// var test = true;
+
+// if (test) {
+//   var total = 'oranges';
+//   console.log(`hello there`);
+// }
+
+// console.log(total);
+
+let total = 1000;
+let test = true;
+
+if (test) {
+  let total = 'oranges';
+  let value = 'dfgsfg';
+  console.log(`hello there`);
+}
+
+console.log(total);
