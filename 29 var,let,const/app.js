1b3f88ec28e582d920a48a8cc317b0a5941fb22a
sunny patel <sunnykpatel.skp@gmail.com>
2023-08-24 12:03:08 -0400

    add es6 lesson var let const complete

--- 29 var,let,const/app.js
+++ 29 var,let,const/app.js
@@ -1,1 +1,33 @@
-//
+//var, let , const
+
+//var========================================
+//define
+var number = 100;
+// console.log(number);
+//update
+number = 200;
+// console.log(number);
+//redefine
+number = 'orange';
+// console.log(number);
+
+//let
+//define
+let ammount = 100;
+// console.log(ammount);
+//update
+ammount = 200;
+// console.log(ammount);
+//redefine
+// let ammount = 'orange';
+
+// //cosnt
+// const total = 100;
+// console.log(total);
+// total = 200;
+// console.log(total);
+
+const person = { name: 'polo' };
+person.name = 'john';
+
+console.log(person.name);
