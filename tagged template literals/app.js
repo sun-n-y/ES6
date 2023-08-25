6e5702ca317a7469ff5c884926851bfbea596519
sunny patel <sunnykpatel.skp@gmail.com>
2023-08-25 15:15:47 -0400

    add es6 tagged template literals lesson complete

--- tagged template literals/app.js
+++ tagged template literals/app.js
@@ -1,1 +1,17 @@
 //
+const author = 'some author';
+const statment = 'some statement';
+
+const quote = highlight`here is the ${statment} by ${author} and it could not be more true`;
+
+console.log(quote);
+
+const result = document.getElementById('result');
+result.innerHTML = quote;
+
+function highlight(text, ...args) {
+  const awesomeText = text.map((item, index) => {
+    return `${item} <strong class="blue">${args[index] || ''}</strong>`;
+  });
+  return awesomeText.join('');
+}
