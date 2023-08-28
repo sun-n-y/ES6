b69d03b976faf610d7c9c3c180e316b47a855880
sunny patel <sunnykpatel.skp@gmail.com>
2023-08-28 12:01:37 -0400

    add es6 array from dom comepelte

--- array from dom/app.js
+++ array from dom/app.js
@@ -1,1 +1,18 @@
 //
+const p = document.querySelectorAll('p');
+const result = document.getElementById('result');
+const second = document.getElementById('second');
+
+console.log(p);
+
+let newText = Array.from(p);
+console.log(newText);
+newText = newText.map((item) => `<span>${item.textContent}</span>`).join(' ');
+
+result.innerHTML = newText;
+
+const text = Array.from(document.querySelectorAll('p'), (item) => {
+  return 'hello';
+});
+
+console.log(text);
