37e27ff7444e4b43fd94775b02c028f6a527dd3f
sunny patel <sunnykpatel.skp@gmail.com>
2023-08-28 11:50:56 -0400

    add es6 array from complete

--- array from/app.js
+++ array from/app.js
@@ -1,1 +1,14 @@
 //
+const udemy = 'udemy';
+
+console.log(Array.from(udemy));
+
+function countTotal() {
+  let total = Array.from(arguments).reduce(
+    (total, currNum) => (total += currNum),
+    0
+  );
+  console.log(total);
+}
+
+countTotal(34, 45, 23, 12, 23);
