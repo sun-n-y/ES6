a00ef2dba1ddeb5d14f993bc4d4c3439e645de29
sunny patel <sunnykpatel.skp@gmail.com>
2023-08-29 12:30:57 -0400

    add es6 set lesson compelte

--- set/app.js
+++ set/app.js
@@ -1,1 +1,22 @@
 //
+
+const unique = new Set();
+
+const random = 'third';
+
+unique.add('first');
+unique.add('sec');
+unique.add('thrd');
+unique.add('first');
+unique.add(4);
+unique.add(random);
+
+const result = unique.delete('third');
+console.log(result);
+
+const isValue = unique.has(4);
+console.log(isValue);
+
+// unique.clear();
+
+console.log(unique);
