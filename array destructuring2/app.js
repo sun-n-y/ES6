619e6a0c7019b9969e20c118874009ba70b5841c
sunny patel <sunnykpatel.skp@gmail.com>
2023-09-23 13:54:59 -0400

    add es6 array swap

--- array destructuring2/app.js
+++ array destructuring2/app.js
@@ -5,3 +5,9 @@ const friends = ['john', 'peter', 'bob', 'anna', 'kelly'];
 
 const [one, , , two] = fruits;
 console.log(one, two);
+
+let first = 'joe';
+let second = 'tim';
+
+[second, first] = [first, second];
+console.log(second);
